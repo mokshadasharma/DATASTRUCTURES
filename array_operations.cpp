2ef1def1ca3134e90b7a843416296743747c559f
mokshada <42065464+mokshadasharma@users.noreply.github.com>
2019-07-30 23:13:56 +0530

    Add files via upload

---
+++ array_operations.cpp
@@ -0,0 +1,67 @@
+/*#include<iostream>
+using namespace std;
+int main()
+{
+//array insertion code
+    /*int n,x,l;
+    cout<<"enter size";
+    cin>>n;
+    cout<<"ele to be inserted";
+    cin>>x;
+    cout<<"enter index";
+    cin>>l;
+    int a[n];
+    cout<<"enter ele in array";
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    for(int j=n;j>=l;j--)
+    {
+        a[j+1]=a[j];
+    }
+    a[l]=x;
+    for(int i=0;i<n+1;i++)
+        cout<<a[i]<<" ";*/
+//array deletion code
+/*int n,x,l;
+    cout<<"enter size";
+    cin>>n;
+    cout<<"enter index of ele to b deleted";
+    cin>>l;
+    int a[n];
+    cout<<"enter ele in array";
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    for(int j=l;j<n;j++)
+    {
+        a[j]=a[j+1];
+    }
+    a[l]=x;
+    for(int i=0;i<n-1;i++)
+        cout<<a[i]<<" ";*/
+//array updation code
+  /*  int n,x,l,j;
+    cout<<"enter size";
+    cin>>n;
+    cout<<"enter  ele to b changed";
+    cin>>l;
+    cout<<"enter ele to b updated";
+    cin>>x;
+    int a[n];
+    cout<<"enter ele in array";
+    for(int i=0;i<n;i++)
+    {
+    cin>>a[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==l)
+        {
+            j=i;
+            a[j]=x;
+        }
+    }
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+*/
+
+
